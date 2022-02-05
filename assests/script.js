24a2be08cc24c12348e1cb924713f94a30d21fd5
AlexNexton <cianoc13@gmail.com>
2022-02-05 18:02:29 +0000

    initial structure of js

---
+++ assests/script.js
@@ -0,0 +1,35 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function additonQ() {
+
+}
+
+function subtractQ() {
+
+}
+
+function multiplyQ() {
+
+}
+
+function divideQ() {
+    
+}
